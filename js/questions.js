06876f05c9e07e5595730504826781da619d1c08
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-08-01 09:36:31 -0300

    Added app files.

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+//Creating an array and passing the number, questions, options, and answers
+let questions = [
+
+
+    {
+        numb: 1,
+        questions: "O termo HTML é referente a:",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ],
+    },
+
+
+
+    {
+        numb: 2,
+        questions: "O termo CSS é referente a:",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Comom Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ],
+    },
+
+
+    {
+        numb: 3,
+        questions: "O termo PHP é referente a:",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Prepocessor"
+        ],
+    },
+
+
+    {
+        numb: 4,
+        questions: "O termo SQL é referente a:",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ],
+    },
+
+
+    {
+        numb: 5,
+        questions: "O termo XML é referente a:",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ],
+    },
+
+
+];
